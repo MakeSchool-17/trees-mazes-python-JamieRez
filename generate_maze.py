0901e2fc93d920467013eb4722a95c3b9420d210
JamieRez <jamierezendes@gmail.com>
2018-05-02 17:22:54 -0700

    Generate and Solve Mazes

--- generate_maze.py
+++ generate_maze.py
@@ -1,11 +1,23 @@
 import maze
-
+import random
 
 # Create maze using Pre-Order DFS maze creation algorithm
 def create_dfs(m):
-    # TODO: Implement create_dfs
-    pass
-
+    backtrack_stack = []
+    rand_cell_index = random.randint(0, len(m.maze_array) - 1)
+    visited_cells = 1
+    while visited_cells < len(m.maze_array):
+        unvisited_neighbors = m.cell_neighbors(rand_cell_index)
+        if len(unvisited_neighbors) >= 1:
+            rand_neighbor = unvisited_neighbors[random.randint(0, len(unvisited_neighbors) - 1)]
+            m.connect_cells(rand_cell_index, rand_neighbor[0], rand_neighbor[1])
+            backtrack_stack.append(rand_cell_index)
+            rand_cell_index = rand_neighbor[0]
+            visited_cells += 1
+        else:
+            rand_cell_index = backtrack_stack.pop()
+        m.refresh_maze_view()
+    m.state = "solve"
 
 def main():
     current_maze = maze.Maze('create')
--- solve_maze.py
+++ solve_maze.py
@@ -1,19 +1,45 @@
 import maze
 import generate_maze
 import sys
+import random
 
 
 # Solve maze using Pre-Order DFS algorithm, terminate with solution
 def solve_dfs(m):
-    # TODO: Implement solve_dfs
-    pass
-
+    stack = []
+    cur_Cell = 0
+    visited_cells = 0
+    while not cur_Cell == len(m.maze_array) - 1:
+        neighbors = m.cell_neighbors(cur_Cell)
+        if len(neighbors) >= 1:
+            new_cell = neighbors[random.randint(0, len(neighbors) - 1)]
+            m.visit_cell(cur_Cell, new_cell[0], new_cell[1])
+            stack.append(cur_Cell)
+            cur_Cell = new_cell[0]
+            visited_cells += 1
+        else:
+            m.backtrack(cur_Cell)
+            cur_Cell = stack.pop()
+        m.refresh_maze_view()
+    m.state = "idle"
 
 # Solve maze using BFS algorithm, terminate with solution
 def solve_bfs(m):
-    # TODO: Implement solve_bfs
-    pass
-
+    queue = []
+    cur_cell = 0
+    in_direction = 0b0000
+    visited_cells = 0
+    queue.insert(0, (cur_cell, in_direction))
+    while not cur_cell == len(m.maze_array) - 1 and len(queue) > 0:
+        cur_cell, in_direction = queue.pop()
+        m.bfs_visit_cell(cur_cell, in_direction)
+        visited_cells += 1
+        m.refresh_maze_view()
+        neighbors = m.cell_neighbors(cur_cell)
+        for neighbor in neighbors:
+            queue.insert(0, neighbor)
+    m.reconstruct_solution(cur_cell)
+    m.state = "idle"
 
 def print_solution_array(m):
     solution = m.solution_array()
